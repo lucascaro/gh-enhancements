cd80dddafe275c07e74cb0bcd9c204430403dbae
lucascaro <lucascaro@gmail.com>
2012-11-14 14:05:26 -0300

    Add initial script that adds links to branches in pull requests

---
+++ gh_enhancements.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name        gh enhancements
+// @namespace   ghplus
+// @description Enhances github pages
+// @include     http://github.com/*
+// @include     https://github.com/*
+// @version     1
+// @grant		none
+// ==/UserScript==
+
+$(function () {
+  var repo = $('a.js-current-repository').html();
+  $('.commit-ref').each(function(){
+    var $this = $(this);
+    var $user = $this.children('.user');
+    var $branch = $this.children().not($user).filter('span');
+    $user = $user.children('span');
+
+    console.log($this);
+    console.log($user);
+    console.log($branch);
+
+    // Make username a link.
+    var username = $user.html();
+    $user.html('<a href="/' + username + '" style="color:#CCCCCC;" >' + username + '</a>');
+
+    // Make branch a link.
+    var branch = $branch.html();
+    var branch_url = '/' + username + '/' + repo + '/tree/' + branch;
+    var branch_link = '<a href="' + branch_url + '" style="color:#ffffff;">' + branch + '</a>'
+    $branch.html(branch_link);
+  });
+});
