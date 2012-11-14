ee058f361b2980d812a66d0eafde0d218779937d
lucascaro <lucascaro@gmail.com>
2012-11-14 14:07:26 -0300

    Changed name

--- gh_enhancements.user.js
+++ gh_enhancements.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        gh enhancements
+// @name        GitHub Custom Enhancements
 // @namespace   ghplus
 // @description Enhances github pages
 // @include     http://github.com/*
